9547b4ce3ff8227578289649c39e74245e050eda
danips2292 <danielpachesan@gmail.com>
2016-06-14 22:11:01 -0600

    strategy para archivos

---
+++ amortizationSystem/src/amortizationsystem/SaveContext.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amortizationsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Daniel
+ */
+public class SaveContext {
+    private SaveStrategy strategy;
+    
+    public SaveContext(SaveStrategy strategy){
+        this.strategy = strategy;
+    }
+    
+    public String executeStrategy(String datos){
+        return strategy.createFile(datos);
+    }
+}
---
+++ amortizationSystem/src/amortizationsystem/SaveStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amortizationsystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel
+ */
+public interface SaveStrategy {
+    public String createFile(String datos);
+}
